8d1ac5c90433a5c900c0a032395b38e550c30bb6
li_nian_chuan <158835962+2471845937@users.noreply.github.com>
2025-03-05 07:08:46 +0800

    data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,32 +1,35 @@
-import numpy as np
 import torch
-from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
-from torch.utils.data import Subset, DataLoader
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
 
-def non_iid_split(num_clients=2, alpha=0.5):
-    """使用Dirichlet分布划分Non-IID数据"""
-    dataset = CIFAR10(root="./data", train=True, download=True, transform=ToTensor())
-    labels = np.array([y for _, y in dataset])
-    class_indices = [np.where(labels == i)[0] for i in range(10)]
-    client_indices = [[] for _ in range(num_clients)]
 
-    # 按Dirichlet分布分配样本
-    for cls_idx in class_indices:
-        proportions = np.random.dirichlet(np.repeat(alpha, num_clients))
-        proportions = (proportions * len(cls_idx)).astype(int)
-        split_points = np.cumsum(proportions)[:-1]
-        np.random.shuffle(cls_idx)
-        splits = np.split(cls_idx, split_points)
-        for c in range(num_clients):
-            if len(splits) > c:
-                client_indices[c].extend(splits[c].tolist())
+def get_datasets(client_id):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-    # 创建客户端数据集
-    client_datasets = [Subset(dataset, indices) for indices in client_indices]
-    return client_datasets
+    train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-def get_testloader():
-    """全局测试集"""
-    testset = CIFAR10(root="./data", train=False, download=True, transform=ToTensor())
-    return DataLoader(testset, batch_size=32)
+    # Split train set into two IID parts
+    total_size = len(train_set)
+    split = total_size // 2
+    generator = torch.Generator().manual_seed(42)
+    splits = [split, total_size - split]
+    train_subsets = random_split(train_set, splits, generator=generator)
+
+    client_train = train_subsets[client_id]
+    client_test = test_set  # Use full test set for evaluation
+
+    return client_train, client_test
+
+
+def get_dataloaders(client_id, batch_size=32):
+    train_data, test_data = get_datasets(client_id)
+
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
