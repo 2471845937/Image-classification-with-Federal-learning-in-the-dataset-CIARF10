23178c7533017d1aca319791c98f0baea5d54a3c
li_nian_chuan <158835962+2471845937@users.noreply.github.com>
2025-03-05 18:27:35 +0800

    client.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import pickle
 import argparse
 import torch
 from torch import nn, optim
-from data_loader import get_dataloaders
+from data_loader import get_datasets
 from model import create_model
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -12,18 +12,24 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class FederatedClient:
     def __init__(self, client_id):
         self.client_id = client_id
-        self.train_loader, self.test_loader = get_dataloaders(client_id)
+        train_data, test_data = get_datasets(client_id)
+        self.train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+        self.test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
         self.model = create_model().to(device)
 
-    def local_train(self, global_params, local_epochs=3):
+    def local_train(self, global_params, local_epochs=5):
         self.model.load_state_dict(global_params)
         optimizer = optim.Adam(self.model.parameters(), lr=0.001, weight_decay=1e-4)
         criterion = nn.CrossEntropyLoss()
 
         self.model.train()
+        total_loss = 0.0
+        total_correct = 0
+        total_samples = 0
+
         for epoch in range(local_epochs):
-            total_loss = 0
-            correct = 0
+            epoch_loss = 0.0
+            epoch_correct = 0
             for data, target in self.train_loader:
                 data, target = data.to(device), target.to(device)
                 optimizer.zero_grad()
@@ -32,23 +38,29 @@ class FederatedClient:
                 loss.backward()
                 optimizer.step()
 
-                total_loss += loss.item()
-                pred = output.argmax(dim=1, keepdim=True)
-                correct += pred.eq(target.view_as(pred)).sum().item()
+                epoch_loss += loss.item() * data.size(0)
+                pred = output.argmax(dim=1)
+                epoch_correct += pred.eq(target).sum().item()
+                total_samples += data.size(0)
 
-            train_loss = total_loss / len(self.train_loader)
-            train_acc = 100. * correct / len(self.train_loader.dataset)
+            total_loss += epoch_loss
+            total_correct += epoch_correct
+
+            # 打印每个epoch的信息
+            epoch_train_loss = epoch_loss / len(self.train_loader.dataset)
+            epoch_train_acc = 100.0 * epoch_correct / len(self.train_loader.dataset)
             print(f"Client {self.client_id} - Epoch {epoch + 1}: "
-                  f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc:.2f}%")
+                  f"Train Loss: {epoch_train_loss:.4f} | Train Acc: {epoch_train_acc:.2f}%")
 
-        return self.model.state_dict()
+        avg_loss = total_loss / total_samples
+        avg_accuracy = 100.0 * total_correct / total_samples
+        return self.model.state_dict(), avg_loss, avg_accuracy
 
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(('192.168.1.203', 12345))
 
         while True:
-            # 等待服务器指令
             try:
                 command = sock.recv(5)
                 if not command:
@@ -76,10 +88,14 @@ class FederatedClient:
                 global_params = pickle.loads(data)
 
                 # 本地训练
-                updated_params = self.local_train(global_params)
+                updated_params, avg_loss, avg_acc = self.local_train(global_params)
 
-                # 发送更新参数
-                data = pickle.dumps(updated_params)
+                # 发送更新参数和指标
+                data = pickle.dumps({
+                    'params': updated_params,
+                    'loss': avg_loss,
+                    'accuracy': avg_acc
+                })
                 sock.sendall(len(data).to_bytes(4, 'big'))
                 sock.sendall(data)
 
